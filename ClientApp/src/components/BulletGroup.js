d8c908d9c214805c2ab455665e5202673767a150
Robert Page <rob@srcapture.com>
2023-01-06 20:41:45 +0000

    Add project files.

---
+++ ClientApp/src/components/BulletGroup.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+export const BulletGroup = (props) => {
+
+    return props.bullets.map(bullet => {
+        switch (bullet.type) {
+            case "group":
+                return <ul><BulletGroup bullets={bullet.bullets} /></ul>;
+            case "text":
+                return <li>{bullet.text}</li>;
+        }
+    })
+
+}
+
---
+++ ClientApp/src/components/Counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { BulletGroup } from './BulletGroup';
+
+export const Counter = () => {
+
+    const bullets = [];
+
+    bullets.push({
+        type: "text",
+        text: "thing"
+    });
+
+    bullets.push({
+        type: "group",
+        bullets: []
+    });
+
+    bullets[1].bullets.push({
+        type: "text",
+        text: "thing2"
+    });
+
+    return <div>
+        <ul><BulletGroup bullets={bullets} /></ul>
+    </div>
+}
