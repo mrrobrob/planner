f54488a5b42d92d0aa8702a3a49f97b286a1a2d3
Robert Page <rob@srcapture.com>
2023-01-11 14:49:48 +0000

    migrate app to typescript

---
+++ ClientApp/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { BulletEditor } from './components/BulletEditor';
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <BulletEditor />
+            </div>
+        </div>
+    );
+}
+
+export default App;
